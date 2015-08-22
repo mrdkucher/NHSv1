e31ba8cef49d1b82bf394935230dc5765a30d5fc
David Kucher <mr.david.kucher@gmail.com>
2015-08-22 16:10:47 -0500

    commit all with delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,10 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
+  def destroy
+    @userdelete = User.find(params[:id]).destroy
+    redirect_to '/users'
+  end
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
