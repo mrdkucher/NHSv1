ab4c1c2678319ed4d53ef99baae5b5b337061122
David Kucher <mr.david.kucher@gmail.com>
2015-10-13 17:32:06 -0500

    pics

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :hours
   get '/leaderboard' => 'leaderboard#index'
   get '/essay' => 'essay#index'
+  resources :essay
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
